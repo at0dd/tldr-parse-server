9bbdc88d50b183e74cc4f7bfd9c7d47765bdd610
at0dd <alex@atodd.io>
2016-11-21 21:22:16 -0600

    Update Links

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A1
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.3 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get update && sudo apt-get install -y mongodb python nodejs build-essential libkrb5-dev
-sudo curl https://github.com/at0dd/tldr-parse-server/blob/master/mongod.service > /lib/systemd/system/mongod.service
+sudo curl https://raw.githubusercontent.com/at0dd/tldr-parse-server/master/mongod.service > /lib/systemd/system/mongod.service
 sudo service mongod start && sudo systemctl enable mongod
 sudo npm install -g parse-server mongodb-runner parse-dashboard pm2
 cd /var/www/
 git clone git@github.com:ParsePlatform/parse-server-example.git
 cd parse-server-example
 npm install
-sudo curl https://github.com/at0dd/tldr-parse-server/blob/master/index.js > index.js
+sudo curl https://raw.githubusercontent.com/at0dd/tldr-parse-server/master/index.js > index.js
 sudo ufw allow 22/tcp && sudo ufw allow 1234/tcp && sudo ufw allow 27017/tcp && sudo ufw enable
 pm2 start index.js && pm2 startup && pm2 save
