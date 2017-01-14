f4d4c68704efbff4120e4fe7f09ad4e93210a2a1
at0dd <alex@atodd.io>
2017-01-14 12:49:55 -0600

    Updated for MongoDB 3.4

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 cd ~
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.3 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-sudo apt-get install -y mongodb python nodejs build-essential libkrb5-dev
-sudo curl https://raw.githubusercontent.com/at0dd/tldr-parse-server/master/mongod.service > /lib/systemd/system/mongod.service
-sudo service mongod start && sudo systemctl enable mongod
+sudo apt-get install -y mongodb-org python nodejs build-essential libkrb5-dev
+sudo service mongod start
 sudo npm install -g parse-server mongodb-runner parse-dashboard pm2
 git clone https://github.com/ParsePlatform/parse-server-example.git
 cd parse-server-example
